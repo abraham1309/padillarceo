365623bac90fa4e0329859ddd518802159057666
abraham38 <abraham@mobil.aullox.com>
2023-11-22 01:13:40 -0600

    Primer commit

---
+++ javascript/carruselInicio.js
@@ -0,0 +1,35 @@
+var swiper = new Swiper('.contenedor-carrusel-inicio', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: "true",
+    // init: false,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+
+    breakpoints: {
+        620: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+        },
+        680: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+        },
+        920: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+        },
+        1240: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+        },
+    },
+
+});
