1620df7fbedcb0a08717aaf04cb63c030b67e10b
abraham38 <abraham@mobil.aullox.com>
2023-12-06 01:41:39 -0600

    Se agrego la funcion de contador

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,4 @@
+/*Script para el navbar*/
 jQuery(function($) {
     $(window).on('scroll', function() {
         if ($(this).scrollTop() >= 200) {
@@ -30,4 +31,41 @@ jQuery(function($) {
     $(window).on('resize', adjustNav);
 
     adjustNav();
+});
+
+/* Script para el contador */
+/* Script para el contador */
+const contadores = document.querySelectorAll('.p-numero');
+const velocidad = 8000;
+const retraso = 1000;
+
+const animarContadores = () => {
+    for (const contador of contadores) {
+        const actualizarContador = () => {
+            let cantidadMaxima = +contador.dataset.cantidadTotal,
+                valorActual = +contador.innerText,
+                incremento = cantidadMaxima / velocidad;
+
+            if (valorActual < cantidadMaxima) {
+                contador.innerHTML = Math.ceil(valorActual + incremento);
+                setTimeout(actualizarContador, 300); // Ajusté el tiempo de espera para que vaya más lento
+            } else {
+                contador.innerText = cantidadMaxima;
+            }
+        };
+        actualizarContador();
+    }
+};
+
+function estaEnLaMitad(elemento) {
+    var rect = elemento.getBoundingClientRect();
+    return rect.top <= window.innerHeight / 2 && rect.bottom >= window.innerHeight / 2;
+}
+
+window.addEventListener('scroll', function() {
+    var miDiv = document.getElementById('seccionDiplomados');
+
+    if (estaEnLaMitad(miDiv)) {
+        setTimeout(animarContadores, retraso);
+    }
 });
